0a6ac1c78fb047650499cbf9eec36e941165c901
KRBeomJunKim <wka8963@naver.com>
2021-11-23 01:24:07 +0900

    project intial setting

---
+++ src/main/java/com/beforedeadline/anbuback/AnbuBackApplication.java
@@ -0,0 +1,13 @@
+package com.beforedeadline.anbuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnbuBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnbuBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beforedeadline/anbuback/AnbuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beforedeadline.anbuback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnbuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
