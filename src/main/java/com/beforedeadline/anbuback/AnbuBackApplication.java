a500f598555c3af24edbd0953bfcb27db8e079d9
KRBeomJunKim <wka8963@naver.com>
2021-11-24 00:30:41 +0900

    버그수정

--- src/main/java/com/beforedeadline/anbuback/AnbuBackApplication.java
+++ src/main/java/com/beforedeadline/anbuback/AnbuBackApplication.java
@@ -2,8 +2,10 @@ package com.beforedeadline.anbuback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AnbuBackApplication {
 
     public static void main(String[] args) {
